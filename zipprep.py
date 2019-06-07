e9121fb134e0af8c2cf28ded40cdc11dca65d952
hiten2 <hiten@riseup.net>
2019-06-07 17:37:53 -0400

    fixed bugs

--- zipprep.py
+++ zipprep.py
@@ -16,22 +16,24 @@ def zip_normalize_path(p):
     return "".join(p)
 
 if __name__ == "__main__":
-    root = tkFileDialog.askdirectory(title = "Choose directory")
-
-    if not root:
-        print "Invalid directory."
-        raw_input("Press Enter to exit...")
-    print "You've selected \"%s\"," % root
-    root_parent = os.path.dirname(root)
-    root_name = os.path.basename(root)
-
-    if not raw_input("""however, this will sanitize all pathnames below the directory:
-are you sure you want to continue? [Y/n] """).strip().upper() == "Y":
-        raw_input("Press Enter to exit...")
-    dirs = []
-    files = []
-
     try:
+        root = tkFileDialog.askdirectory(title = "Choose directory")
+
+        if not root:
+            print "Invalid directory."
+            raw_input("Press Enter to exit...")
+            sys.exit(1)
+        print "You've selected \"%s\"," % root
+        root_parent = os.path.dirname(root)
+        root_name = os.path.basename(root)
+
+        if not raw_input("""however, this will sanitize all pathnames below the directory:
+    are you sure you want to continue? [Y/n] """).strip().upper() == "Y":
+            raw_input("Press Enter to exit...")
+            sys.exit(1)
+        dirs = []
+        files = []
+        
         for r, ds, fs in os.walk(root, topdown = False):
             dirs += [os.path.join(r, d) for d in ds]
             files += [os.path.join(r, f) for f in fs]
